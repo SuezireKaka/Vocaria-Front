86d8ffafd33bba2a7eb566e4907e341e28111559
SuezireKaka <rwccm3241@naver.com>
2024-08-04 13:39:30 +0900

    백엔드에서 받은 form 데이터를 InputLine화

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,6 +4,7 @@ import AppContext from "../contexts/AppContextProvider";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import { Button } from 'react-bootstrap';
+import InputLine from "../widgets/form/InputLine";
 
 export default function Register() {
     const { registerForm } = useContext(AppContext);
@@ -12,13 +13,8 @@ export default function Register() {
         <fieldset>
             <legend>회원 가입</legend>
             <Form style={{width:"60%", margin:"0"}}>
-                <InputGroup className="mb-3" style={{
-                    display: "inline-block",
-                    align: "center", width: "60%", backgroundColor: ""
-                }}/>
-
                 {registerForm?.inputPieceList.map((piece, idx) => {
-                    return <Button key = {idx}>{piece.title}</Button>
+                    return <InputLine key = {idx} piece = {piece}/>;
                 })}
             </Form>
     
