608f1748d97c59015cbc59c9d4d309216b34542c
SuezireKaka <rwccm3241@naver.com>
2024-08-15 18:53:36 +0900

    useFetch 컴포넌트화로 Fetch 적용

---
+++ src/shared/hooks/Fetch.js
@@ -0,0 +1,53 @@
+import AppContext from "../context/AppContextProvider";
+import {useFetch, usePost, useAuth} from "./useFetch"
+import { useContext } from "react";
+import Loading from '../layout/Loading';
+
+function Fetch({uri, renderSuccess = f => f,
+    loadingFallBack = <Loading />,
+    renderError = ({error})=>(<pre>{JSON.stringify(error, null, 2)}</pre>),
+    doLog = false}) {
+
+    const {loading, data, error} = useFetch(uri, doLog);
+
+    if (doLog) {
+        console.log("어디 한 번 해보자");
+        console.log(uri, loading, data);
+    }
+
+    if (loading) return loadingFallBack;
+    if (error) return renderError({error});
+    if (data) {
+        return renderSuccess(data);
+    }
+}
+
+function AxiosPost({uri, body, renderSuccess = f=>f,
+    loadingFallBack = <Loading />,
+    renderError = ({error})=>(<pre>{JSON.stringify(error, null, 2)}</pre>)}) {
+
+    const {loading, data, error} = usePost(uri, body);
+
+    if (loading) return loadingFallBack;
+    if (error) return renderError({error});
+    if (data) {
+        return renderSuccess(body, data);
+    }
+}
+
+function AxiosAuth({uri, renderSuccess = f=>f,
+    loadingFallBack = <Loading />,
+    renderError = ({error})=>(<pre>{JSON.stringify(error, null, 2)}</pre>)}) {
+
+    const {auth} = useContext(AppContext)
+
+    const {loading, data, error} = useAuth(uri, auth);
+
+    if (loading) return loadingFallBack;
+    if (error) return renderError({error});
+    if (data) {
+        return renderSuccess(data);
+    }
+}
+
+export {Fetch, AxiosPost, AxiosAuth};
