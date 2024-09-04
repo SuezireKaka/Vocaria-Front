8465261bf03e0946f89ddcf33135eb94fe77e0c6
SuezireKaka <rwccm3241@naver.com>
2024-09-04 18:11:14 +0900

    GroupScopeSpel 테스트

--- src/pages/test/Test.js
+++ src/pages/test/Test.js
@@ -27,6 +27,18 @@ async function fetch2(auth, setData = f => f, setError = f => f) {
     .catch(e => setError(e.message));
 }
 
+async function fetch3(auth, setData = f => f, setError = f => f) {
+    await axios.get("/mission/testSpel/rwccm3241-samplestudent",
+    {
+        headers: {
+            'Content-Type': 'application/json',
+            "x-auth-token": `Bearer ${auth?.accessToken}`
+        }
+    }
+    ).then(setData)
+    .catch(e => setError(e.message));
+}
+
 export default function TokenTest() {
     const {auth} = useContext(AppContext);
 
@@ -48,6 +60,12 @@ export default function TokenTest() {
         <br/>
         <Button
             onClick={() => fetch2(auth, setData, setError)}
+        >
+            {data ? data.data : error}
+        </Button>
+        <br/>
+        <Button
+            onClick={() => fetch3(auth, setData, setError)}
         >
             {data ? data.data : error}
         </Button>
